56ebc17d6bdce44cf22d6ade612206bef73c30f3
Harry Kay Ablor <harrykayablor@Harrys-MacBook-Air.local>
2022-06-26 13:47:58 +0000

    serverless

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = '38ry2ps71e';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-harrykay.us.auth0.com',
+  clientId: 'TRUGlZEeHu1hAiopvY3qWhMjHNdQVsws',
+  callbackUrl: 'http://localhost:3000/callback'
+};
+ 
